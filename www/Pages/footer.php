a15e9c3e49929eeb07afc2cf10c52cf8178606a9
Webdesk-me <atelford@webdesk.me>
2018-05-02 13:33:01 -0400

    Create footer.php

---
+++ www/Pages/footer.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-inverse navbar-fixed-bottom">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#footNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span> 
+      </button>
+    </div>
+    <div class="collapse navbar-collapse" id="footNavbar">
+    <ul class="nav navbar-nav">
+      <li><a href="#"><i class="fa fa-copyright" aria-hidden="true"></i><?php echo date("Y"); ?>, All Rights Reserved
+</a></li>
+      <li><a href="www/Pages/Terms.html">Terms Of Use</a></li>
+      <li><a href="www/Pages/Privacy.html">Privacy Policey</a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li>Proudly powerd by <a href="webdesk.me">WebDesk</a></li>
+    </ul>
+    </div>
+  </div>
+</nav>
