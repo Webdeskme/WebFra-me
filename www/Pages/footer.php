375c03b124910e8aeb21bfec827e38cf40ede9cb
Webdesk-me <atelford@webdesk.me>
2018-05-02 13:42:40 -0400

    Update footer.php

--- www/Pages/footer.php
+++ www/Pages/footer.php
@@ -9,7 +9,7 @@
     </div>
     <div class="collapse navbar-collapse" id="footNavbar">
     <ul class="nav navbar-nav">
-      <li><a href="#"><i class="fa fa-copyright" aria-hidden="true"></i><?php echo date("Y"); ?>, All Rights Reserved
+      <li><a href="#"><i class="fa fa-copyright" aria-hidden="true"></i><?php echo date("Y") . ' ' . $wd_Title; ?>, All Rights Reserved
 </a></li>
       <li><a href="www/Pages/Terms.html">Terms Of Use</a></li>
       <li><a href="www/Pages/Privacy.html">Privacy Policey</a></li>
