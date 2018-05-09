ba7bd93daf15672b5ecb5a942b89bfade7034085
Webdesk-me <info@webdesk.me>
2018-05-09 11:26:26 -0400

    Spelling
    Spelling

--- www/Pages/footer.php
+++ www/Pages/footer.php
@@ -4,7 +4,7 @@
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#footNavbar">
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
-        <span class="icon-bar"></span> 
+        <span class="icon-bar"></span>
       </button>
     </div>
     <div class="collapse navbar-collapse" id="footNavbar">
@@ -12,7 +12,7 @@
       <li><a href="https://www.copyright.gov/title17/" target="_blank"><i class="fa fa-copyright" aria-hidden="true"></i><?php echo date("Y") . ' ' . $wd_Title; ?>, All Rights Reserved
 </a></li>
       <li><a href="www/Pages/Terms.html" target="_blank">Terms Of Use</a></li>
-      <li><a href="www/Pages/Privacy.html" target="_blank">Privacy Policey</a></li>
+      <li><a href="www/Pages/Privacy.html" target="_blank">Privacy Policy</a></li>
     </ul>
     <ul class="nav navbar-nav navbar-right">
       <li><a href="http://webdesk.me" target="_blank">Proudly powerd by WebDesk</a></li>
