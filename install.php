ef5c644ae23264af46579aaa4b50bdc8715c0f24
Webdesk-me <info@webdesk.me>
2018-05-09 11:09:51 -0400

    Fix Install
    Fix Install

--- install.php
+++ install.php
@@ -10,7 +10,7 @@ $wd_path ="";
     $characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
     $string = '';
  $max = strlen($characters) - 1;
- for ($i = 0; $i < $random_string_length; $i++) {
+ for ($i = 0; $i < 16; $i++) {
       $string .= $characters[mt_rand(0, $max)];
  }
 ?>
