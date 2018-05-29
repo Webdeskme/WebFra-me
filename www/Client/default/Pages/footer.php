6debb034274dd4535f771e06a22d88a948bf4615
Webdesk-me <info@webdesk.me>
2018-05-29 12:28:00 -0400

    security prep update
    security prep update

---
+++ www/Client/default/Pages/footer.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-inverse navbar-fixed-bottom">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#footNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    </div>
+    <div class="collapse navbar-collapse" id="footNavbar">
+    <ul class="nav navbar-nav">
+      <li><a href="https://www.copyright.gov/title17/" target="_blank"><i class="fa fa-copyright" aria-hidden="true"></i> <?php echo date("Y") . ' ' . $wd_Title; ?>, All Rights Reserved
+</a></li>
+      <li><a href="www/Pages/Terms.html" target="_blank">Terms Of Use</a></li>
+      <li><a href="www/Pages/Privacy.html" target="_blank">Privacy Policy</a></li>
+      <li><a href="www/Pages/community-covenant.html" target="_blank">Community Covenant</a></li>
+      <li><a href="index.php?page=blog.php">Blog</a></li>
+      <li><a href="feed.xml" target="_blank">RSS Feed <i class="fas fa-rss"></i></a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="http://webdesk.me" target="_blank">Proudly powerd by WebDesk</a></li>
+    </ul>
+    </div>
+  </div>
+</nav>
---
+++ www/Client/default/Pages/header.php
@@ -0,0 +1,1 @@
+<title><?php echo $wd_Title; ?></title>
