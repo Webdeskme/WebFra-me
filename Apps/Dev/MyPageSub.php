b5b1b9ca47e590d694e58c4c13f5238dae75ca4e
Webdesk-me <atelford@webdesk.me>
2018-05-31 14:47:40 -0400

    Update MyPageSub.php

--- Apps/Dev/MyPageSub.php
+++ Apps/Dev/MyPageSub.php
@@ -1,7 +1,7 @@
 <?php
 $nameA = test_input($_POST["nameA"]);
 $nameP = test_input($_POST["nameP"]);
-$con = htmlspecialchars_decode($_POST["con"], ENT_QUOTES);
+$con = htmlspecialchars_decode($wd_POST["con"], ENT_QUOTES);
 file_put_contents("MyApps/" . $nameA . "/" . $nameP, $con);
 wd_head($wd_type, $wd_app, 'MyPage.php', '&MyApp=' . $nameA . '&MyPage=' . $nameP);
 ?>
