95155b1cb1bf0847597edadb03959913596a58c1
Webdesk-me <atelford@webdesk.me>
2018-05-31 14:30:00 -0400

    Update MyPageSub.php

--- Apps/Terminal/MyPageSub.php
+++ Apps/Terminal/MyPageSub.php
@@ -3,7 +3,7 @@
 //include("testInput.php");
 $dir = test_input($_POST["dir"]);
 $file = test_input($_POST["file"]);
-$con = htmlspecialchars_decode($_POST["con"], ENT_QUOTES);
+$con = htmlspecialchars_decode($wd_POST["con"], ENT_QUOTES);
 file_put_contents($_SESSION['root'] . $dir . $file, $con);
 wd_head($wd_type, $wd_app, 'MyPage.php', '&dir=' . $dir . '&file=' . $file . '&wd_as=Saved!');
 ?>
