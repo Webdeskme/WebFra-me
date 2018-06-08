41b43e105b433bf19aec34c82658da89ed5f7945
Webdesk-me <atelford@webdesk.me>
2018-06-08 12:59:49 -0400

    Update startSub.php

--- Apps/Dev/startSub.php
+++ Apps/Dev/startSub.php
@@ -1,7 +1,7 @@
 <?php
 $nameA = test_input($_POST["nameA"]);
 mkdir("MyApps/" . $nameA);
-file_put_contents("MyApps/" . $nameA . "/start.php", '<?php ?>');
-file_put_contents("MyApps/" . $nameA . "/header.php", '<link rel="stylesheet" href="Plugins/bootstrap/css/bootstrap.min.css">' . "\n" . '<script src="Plugins/bootstrap/js/bootstrap.min.js"></script>');
+file_put_contents("MyApps/" . $nameA . "/start.php", '<?php include_once "../../wd_protect.php"; ?>');
+file_put_contents("MyApps/" . $nameA . "/header.php", '<?php include_once "../../wd_protect.php"; ?><link rel="stylesheet" href="Plugins/bootstrap/css/bootstrap.min.css">' . "\n" . '<script src="Plugins/bootstrap/js/bootstrap.min.js"></script>');
 wd_head($wd_type, $wd_app, 'MyPage.php', '&MyApp=' . $nameA . '&MyPage=start.php');
 ?>
