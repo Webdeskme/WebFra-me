fbdf8d05341c9f6163a40257f7f6c67e48da931e
Webdesk-me <atelford@webdesk.me>
2018-06-08 13:10:00 -0400

    Update wd_protect.php

--- wd_protect.php
+++ wd_protect.php
@@ -1,7 +1,12 @@
 <?php
-if(isset($wd_protect) && $wd_protect == 'yes'){
+if(isset($wd_protect) && $wd_protect != 'yes'){
   session_destroy();
-  header('Location: index.php');
+  header('HTTP/1.1 404 Not Found');
+  exit();
+}
+elseif(!isset($wd_protect)){
+  session_destroy();
+  header('HTTP/1.1 404 Not Found');
   exit();
 }
 ?>
