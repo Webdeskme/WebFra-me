4412236ea071c30062c4b4bb51427c99d76a8e1d
Webdesk-me <atelford@webdesk.me>
2018-06-08 12:58:19 -0400

    Update MyAppSub.php

--- Apps/Dev/MyAppSub.php
+++ Apps/Dev/MyAppSub.php
@@ -1,6 +1,6 @@
 <?php
 $nameA = test_input($_POST["nameA"]);
 $nameP = test_input($_POST["nameP"]);
-file_put_contents("MyApps/" . $nameA . "/" . $nameP, '<?php ?>');
+file_put_contents("MyApps/" . $nameA . "/" . $nameP, '<?php include_once "../../wd_protect.php"; ?>');
 wd_head($wd_type, $wd_app, 'MyPage.php', '&MyApp=' . $nameA . '&MyPage=' . $nameP);
 ?>
