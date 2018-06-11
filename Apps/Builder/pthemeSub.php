25e7f18bbb98e32b6df048cd51c30a7921821205
Webdesk-me <info@webdesk.me>
2018-06-10 21:36:33 -0400

    Theme Update
    Theme Update

--- Apps/Builder/pthemeSub.php
+++ Apps/Builder/pthemeSub.php
@@ -1,5 +1,5 @@
 <?php if(is_file("../../wd_protect.php")){ include_once "../../wd_protect.php"; }
 $theme = test_input($_GET['theme']);
-file_put_contents("www/dtheme.txt", $theme);
+file_put_contents($wd_root . "/Admin/dtheme.txt", $theme);
 wd_head($wd_type, $wd_app, 'pthemes.php', '&wd_as=Your theme was successfuly changed!');
 ?>
