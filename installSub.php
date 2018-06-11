773dfbcdccb49c121ebc7ffb6a5575890d7ee032
Webdesk-me <info@webdesk.me>
2018-06-11 11:44:51 -0400

    fix
    fix

--- installSub.php
+++ installSub.php
@@ -76,7 +76,7 @@ $wd_roots[$_SERVER['HTTP_HOST']] = $path;
                         //App Web Files
                         mkdir('web/' . $arand . '/');
                         //file_put_contents('../../webdesk/User/' . $user .'/Admin/email.txt', t_enc($_SESSION["email"]));
-                        file_put_contents($path . '/User/' . $user .'/Admin/pass.txt', up_enc($pass . $user . $prand));
+                        file_put_contents($path . '/User/' . $user .'/Admin/pass.txt', up_enc($pass . $user . test_input($prand)));
                         file_put_contents($path . '/User/' . $user .'/Admin/oid.txt', $rand);
                         file_put_contents($path . '/User/' . $user .'/Admin/prand.txt', $prand);
                         file_put_contents($path . '/User/' . $user .'/Admin/back.txt', 'back.jpg');
