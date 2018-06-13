94d8d3e0031dd7ba653631f60f1d41672f2d89e6
Webdesk-me <info@webdesk.me>
2018-06-13 10:02:47 -0400

    cache

--- www/Themes/wd_default/pluginCSS.php
+++ www/Themes/wd_default/pluginCSS.php
@@ -1,12 +1,19 @@
 <?php
 header("Content-type: text/css; charset: UTF-8");
-//string session_cache_limiter ('public');
-$seconds_to_cache = 86400;
+$a = array("../../../Plugins/bootstrap-3.3.7-dist/css/bootstrap.min.css", "../../../Theme/default.php", "style.css");
+$seconds_to_cache = 86400 * 7;
 $ts = gmdate("D, d M Y H:i:s", time() + $seconds_to_cache) . " GMT";
 header("Expires: $ts");
 header("Pragma: cache");
 header("Cache-Control: public, max-age=$seconds_to_cache");
-echo file_get_contents("../../../Plugins/bootstrap-3.3.7-dist/css/bootstrap.min.css");
-echo file_get_contents("../../../Theme/default.php");
-echo file_get_contents("style.css");
+$last_modified_time = "";
+foreach ($a as $value) {
+  $last_modified_time = $last_modified_time . filemtime($value);
+}
+$etag = md5_file("pluginCSS.php" . $last_modified_time);
+//header("Last-Modified: ".gmdate("D, d M Y H:i:s", $last_modified_time)." GMT");
+header("Etag: $etag");
+foreach ($a as $value) {
+  echo file_get_contents($value);
+}
 ?>
--- www/Themes/wd_default/plugins.php
+++ www/Themes/wd_default/plugins.php
@@ -1,11 +1,18 @@
 <?php
-//
-$seconds_to_cache = 86400;
+$a = array("../../../Plugins/jquery.min.js", "../../../Plugins/bootstrap-3.3.7-dist/js/bootstrap.min.js", "../../../Plugins/fontawesome-free/svg-with-js/js/fontawesome-all.min.js");
+$seconds_to_cache = 86400 * 7;
 $ts = gmdate("D, d M Y H:i:s", time() + $seconds_to_cache) . " GMT";
 header("Expires: $ts");
 header("Pragma: cache");
 header("Cache-Control: public, max-age=$seconds_to_cache");
-echo file_get_contents("../../../Plugins/jquery.min.js");
-echo file_get_contents("../../../Plugins/bootstrap-3.3.7-dist/js/bootstrap.min.js");
-echo file_get_contents("../../../Plugins/fontawesome-free/svg-with-js/js/fontawesome-all.min.js");
+$last_modified_time = "";
+foreach ($a as $value) {
+  $last_modified_time = $last_modified_time . filemtime($value);
+}
+$etag = md5_file("pluginCSS.php" . $last_modified_time);
+//header("Last-Modified: ".gmdate("D, d M Y H:i:s", $last_modified_time)." GMT");
+header("Etag: $etag");
+foreach ($a as $value) {
+  echo file_get_contents($value);
+}
 ?>
