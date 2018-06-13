d3d2a50ea2234a2525b45d39727345143cdbb805
Webdesk-me <info@webdesk.me>
2018-06-13 10:39:21 -0400

    feed

--- feed.php
+++ feed.php
@@ -1,6 +1,6 @@
 <?php
 header('Content-Type: application/rss+xml; charset=utf-8');
 include "testInput.php";
-echo test_input(file_get_contents($wd_www . 'feed.xml'));
+eadfile($wd_www . 'feed.xml');
 exit();
 ?>
