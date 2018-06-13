7fa74b86de30c937ef397ec6bb43c7c25bd48afd
Webdesk-me <info@webdesk.me>
2018-06-13 11:14:53 -0400

    fix

--- www/Themes/wd_default/plugins.php
+++ www/Themes/wd_default/plugins.php
@@ -2,7 +2,7 @@
 include "../../../testInput.php";
 $page = test_input($_GET['page']);
 $a = array("../../../Plugins/jquery.min.js", "../../../Plugins/bootstrap-3.3.7-dist/js/bootstrap.min.js", "../../../Plugins/fontawesome-free/svg-with-js/js/fontawesome-all.min.js", "fstat-js.php?page=" . $page);
-$seconds_to_cache = 86400 * 7;
+$seconds_to_cache = 86400 * 30;
 $ts = gmdate("D, d M Y H:i:s", time() + $seconds_to_cache) . " GMT";
 header("Expires: $ts");
 header("Pragma: cache");
