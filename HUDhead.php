6c3000a30cc8b28e3e4afe45ecaf7e53f37eec4a
Webdesk-me <info@webdesk.me>
2018-06-28 12:31:49 -0400

    async

--- HUDhead.php
+++ HUDhead.php
@@ -14,19 +14,19 @@
     <link rel="apple-touch-icon" href="favicon.ico">
     <link rel="apple-touch-startup-image" href="favicon.ico">
 <link rel="manifest" href="manifest.php">
-<link rel="stylesheet" href="Plugins/wd-bootstrap/css/webdesk_bootstrap.min.css">
-<link rel="stylesheet" href="Plugins/jquery-ui/jquery-ui.min.css">
-<link rel="stylesheet" type="text/css" href="Plugins/context.standalone.css">
-<link href="Plugins/fullcalendar/fullcalendar.min.css" rel="stylesheet"/>
-<link href="Plugins/fullcalendar/fullcalendar.print.min.css" rel="stylesheet" media="print" />
-<link rel="stylesheet" type="text/css" href="Theme/default.php">
-<script src="Plugins/jquery.min.js"></script>
-<script src="Plugins/wd-bootstrap/js/webdesk_bootstrap.js"></script>
-<script src="Plugins/jquery-ui/jquery-ui.min.js"></script>
-<script src="Plugins/tinymce/js/tinymce/tinymce.min.js"></script>
-<script src="Plugins/fullcalendar/lib/moment.min.js"></script>
-<script src="Plugins/fullcalendar/fullcalendar.min.js"></script>
-<script defer src="Plugins/fontawesome-free/svg-with-js/js/fontawesome-all.min.js"></script>
+<link rel="stylesheet" href="Plugins/wd-bootstrap/css/webdesk_bootstrap.min.css" async>
+<link rel="stylesheet" href="Plugins/jquery-ui/jquery-ui.min.css"  async>
+<link rel="stylesheet" type="text/css" href="Plugins/context.standalone.css"  async>
+<link href="Plugins/fullcalendar/fullcalendar.min.css" rel="stylesheet"  async/>
+<link href="Plugins/fullcalendar/fullcalendar.print.min.css" rel="stylesheet" media="print" async/>
+<link rel="stylesheet" type="text/css" href="Theme/default.php" async>
+<script src="Plugins/jquery.min.js" defer></script>
+<script src="Plugins/wd-bootstrap/js/webdesk_bootstrap.js" defer></script>
+<script src="Plugins/jquery-ui/jquery-ui.min.js" defer></script>
+<script src="Plugins/tinymce/js/tinymce/tinymce.min.js" defer></script>
+<script src="Plugins/fullcalendar/lib/moment.min.js" defer></script>
+<script src="Plugins/fullcalendar/fullcalendar.min.js" defer></script>
+<script defer src="Plugins/fontawesome-free/svg-with-js/js/fontawesome-all.min.js" defer></script>
 <?php
 include 'wd_ch.php';
 if(isset($_GET["app"])){
@@ -48,4 +48,3 @@ if(isset($_GET["app"])){
 		}
     }
 ?>
-
