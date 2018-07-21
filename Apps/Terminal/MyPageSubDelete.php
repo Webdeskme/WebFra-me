ac77d5b1b8510231d1cad68eddf97dcba6e4bd3c
Webdesk-me <atelford@webdesk.me>
2018-07-21 07:03:35 -0400

    Update MyPageSubDelete.php

--- Apps/Terminal/MyPageSubDelete.php
+++ Apps/Terminal/MyPageSubDelete.php
@@ -1,13 +1,11 @@
 <?php if(is_file("../../wd_protect.php")){ include_once "../../wd_protect.php"; }
-//session_start();
-//include("testInput.php");
 if(isset($_GET["dir"])){$dir = test_input($_GET["dir"]);}
 else{$dir = "";}
 $file = test_input($_GET["file"]);
 unlink($_SESSION['root'] . $dir . $file);
 if($dir != ""){$dir = rtrim($dir, '/');
-header('Location: desktop.html?type=Apps&app=Terminal&sec=start.php&dir=' . $dir);}
+header('Location: desktop.php?type=Apps&app=Terminal&sec=start.php&dir=' . $dir);}
 else{
-header('Location: desktop.html?type=Apps&app=Terminal&sec=start.php');
+header('Location: desktop.php?type=Apps&app=Terminal&sec=start.php');
 }
 ?>
