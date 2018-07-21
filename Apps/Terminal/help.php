44a27caf32f2dfb20c34a4821c4df0d15617eddf
Webdesk-me <atelford@webdesk.me>
2018-07-21 07:00:49 -0400

    Update help.php

--- Apps/Terminal/help.php
+++ Apps/Terminal/help.php
@@ -1,2 +1,5 @@
 <?php if(is_file("../../wd_protect.php")){ include_once "../../wd_protect.php"; } ?>
-<p>Terminal gives is a great program that gives developers access to view and make edits to their files.</p>
+<p>Terminal is a great program that gives developers access to view and make edits to their files.</p>
+<p>Navigate the filesystem as you would with any filesystem by clicking on a directory or file.</p>
+<p>To create a new file or directory, click new, select file or directory, and enter a name.</p>
+<p>To delete a file or directory, navigate into the file or directory, then click delete.</p>
