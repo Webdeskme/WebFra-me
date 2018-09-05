e8f317c54504c99647cdd3c71857a28ec0f69f9b
Andrew McCallister <andrewt834@gmail.com>
2018-09-05 14:08:23 -0400

    Remove Admin

--- Apps/Admin/mediaDelete.php
+++
@@ -1,9 +0,0 @@
-<?php if(is_file("../../wd_protect.php")){ include_once "../../wd_protect.php"; }
-if(isset($_GET['media'])){
-  $media = test_input($_GET['media']);
-  if($media != ""){
-    unlink("www/Media/" . $media);
-  }
-}
-wd_head($wd_type, $wd_app, 'pmedia.php', '');
-?>
--- Apps/Admin/pageSubDelete.php
+++
@@ -1,9 +0,0 @@
-<?php if(is_file("../../wd_protect.php")){ include_once "../../wd_protect.php"; }
-if(isset($_GET['page'])){
-  $page = test_input($_GET['page']);
-  if($page != ""){
-    unlink("www/Pages/" . $page);
-  }
-}
-wd_head($wd_type, $wd_app, 'start.php', '');
-?>
